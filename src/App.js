d8c159d78de4762fd70667b71d3d3faf4c067735
eggleebee <imimraniya@gmail.com>
2024-06-15 16:40:34 +0900

    스킬영역 완료

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Link, Navigate, Route, Routes } from "react-router-dom";
 import HomeView from "./views/HomeView";
+import FirstPage from './FirstPage';
+import Second from './Second';
+import About from "./components/About";
+import Works from "./components/Works";
 
 const App = () => {
+
     return (
         <BrowserRouter>
+            <ul style={{position:"absolute", bottom: 0, zIndex: 10001, backgroundColor:"black", color:"white"}}>
+                <li><Link to="/">포트폴리오</Link></li>
+                <li><Link to="/first">첫번째</Link></li>
+                <li><Link to="/second">두번째</Link></li>
+            </ul>
             <Routes>
                 <Route path="/" element={<HomeView />} />
+                
+                <Route path="/first" element={<FirstPage />} />
+                <Route path="/second" element={<Second />} />
+
+                <Route path="/about" element={<About />} />
+                <Route path="/works" element={<Works />} />
+
+                <Route path="/a" element={<Works />} />
+                <Route path="/w" element={<Works />} />
             </Routes>
         </BrowserRouter>
     );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { headerNav } from "../constants/data"
+import { Link } from "react-router-dom";
 
 console.log(headerNav)
 
@@ -19,12 +20,21 @@ const Header = () => {
             <a href="/">portfolio<em>react.js</em></a>
           </h1>
         </div>
+        
+        {/* 라우팅 & Link 테스트 */}
+        <div>
+          <div><Link to="/a">소개</Link></div>
+          <div><Link to="/w">작업물</Link></div>
+        </div>
+        {/* 라우팅 & Link 테스트 */}
+
         <nav
           className={`header__nav ${show ? "show" : ""}`}
           role="navigation"
           aria-label="메인메뉴"
         >
           <ul>
+
             {
               headerNav.map((nav, key) => (
                 <li key={key}>
@@ -43,7 +53,7 @@ const Header = () => {
           tabIndex="0"
 
           /* onClick={() => setShow((prevShow) => !prevShow)} */
-          
+
           onClick={toggleMenu}
         >
           <span></span>
