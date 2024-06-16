e40adf912d521be5c7c1dd01fe1c0b5a4ec54014
eggleebee <imimraniya@gmail.com>
2024-06-16 17:45:50 +0900

    App.js파일 수정

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React, { useEffect } from "react";
-import { BrowserRouter, Link, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomeView from "./views/HomeView";
+
 import smooth from "./utils/smooth"
 import link from "./utils/link"
-
-
-import FirstPage from './FirstPage';
-import Second from './Second';
-import About from "./components/About";
-import Works from "./components/Works";
-import Intro from "./components/Intro";
 
 const App = () => {
 
@@ -19,7 +13,6 @@ const App = () => {
     }, []);
 
     return (
-
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<HomeView />} />
