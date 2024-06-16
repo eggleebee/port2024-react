c4a39ae6a4413a3a6c850db0dd3b4bb4138ffb5b
eggleebee <imimraniya@gmail.com>
2024-06-16 14:48:51 +0900

    [utils] links.js, smooth.js 기능추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Link, Navigate, Route, Routes } from "react-router-dom";
 import HomeView from "./views/HomeView";
+import smooth from "./utils/smooth"
+import link from "./utils/link"
+
+
 import FirstPage from './FirstPage';
 import Second from './Second';
 import About from "./components/About";
 import Works from "./components/Works";
 
 const App = () => {
+
+    useEffect(() => {
+        smooth();
+        link();
+    }, []);
 
     return (
 
