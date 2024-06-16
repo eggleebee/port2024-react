f8fccb8c6e0569db6f4b785f82d3754f7cf3008c
eggleebee <imimraniya@gmail.com>
2024-06-16 17:21:13 +0900

    App.js파일 수정

--- src/App.js
+++ src/App.js
@@ -20,22 +20,8 @@ const App = () => {
     return (
 
         <BrowserRouter>
-            <ul style={{ position: "absolute", bottom: 0, zIndex: 10001, backgroundColor: "black", color: "white" }}>
-                <li><Link to="/">포트폴리오</Link></li>
-                <li><Link to="/first">첫번째</Link></li>
-                <li><Link to="/second">두번째</Link></li>
-            </ul>
             <Routes>
                 <Route path="/" element={<HomeView />} />
-
-                <Route path="/first" element={<FirstPage />} />
-                <Route path="/second" element={<Second />} />
-
-                <Route path="/about" element={<About />} />
-                <Route path="/works" element={<Works />} />
-
-                <Route path="/a" element={<Works />} />
-                <Route path="/w" element={<Works />} />
             </Routes>
         </BrowserRouter>
     );
